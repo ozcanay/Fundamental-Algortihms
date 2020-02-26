a8976f8ffa4dc3fa1286ee9af5b8cd24d2dcd290
Aydin Ozcan <aydin.ozcan@siemens.com>
2020-02-26 21:13:03 +0300

    iterative binary search

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <vector>
 
-using namespace std;
+/// Iterative approach
+bool binarySearch(const std::vector<int>& nums, int val)
+{
+    int lo = 0;
+    int hi = nums.size() - 1;
+
+    while(lo <= hi) {
+        int mid = lo + (hi - lo)/2;
+
+        if(val < nums[mid]) {
+            hi = mid - 1;
+        } else if(val > nums[mid]) {
+            lo = mid + 1;
+        } else {
+            return true;
+        }
+    }
+
+    return false;
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    std::vector<int>m{1,2,3,4,5,6};
+    std::cout << std::boolalpha << binarySearch(m, 4) << std::endl;
 }
